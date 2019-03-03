58f16aac2a3bf71432522013930a11ecd33e328e
Gareth Evans <evans.g.p@gmail.com>
2019-03-03 15:30:21 +0800

    hello world eg

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
-use cortex_m::asm;
+extern crate stm32f1xx_hal;
+
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{hprintln};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    hprintln!("Hello, world!").unwrap();
 
-    loop {
-        // your code goes here
-    }
+    loop {}
 }
